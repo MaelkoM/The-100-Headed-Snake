257c3a4c2eadba127fc6d9f7d786108747923ee8
Michael Mallok <62209075+MaelkoM@users.noreply.github.com>
2021-02-06 13:51:15 +0100

    artwork updates
    updated artworks for Day 28 and 31

--- Day 28 - Pomodoro App/main.py
+++ Day 28 - Pomodoro App/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -9,98 +10,14 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+TOMATO_START = "Artwork/susa_tomata_start.png"
+TOMATO_WORK = "Artwork/susa_tomata_work.png"
+TOMATO_LONG = "Artwork/susa_tomata_long.png"
+TOMATO_SHORT_LIST = ["Artwork/susa_tomata_short_1.png", "Artwork/susa_tomata_short_2.png"]
 reps = 0
 check_num = ""
 timer = None
 reset = False
-
-# # ---------------------------- TIMER RESET ------------------------------- #
-#
-#
-# def reset_timer():
-#     global reps
-#     global check_num
-#     timer_text.configure(text="25:00")
-#     timer_label.configure(text="Timer", fg=GREEN)
-#     reps = 0
-#     window.after_cancel(timer)
-#     check_num = ""
-#     start_button["state"] = NORMAL
-#
-# # ---------------------------- TIMER MECHANISM ------------------------------- #
-#
-#
-# def start_all():
-#     start_button["state"] = DISABLED
-#     start_timer()
-#
-#
-# def start_timer():
-#     global reps
-#     global check_num
-#     reps += 1
-#     if reps == 8:
-#         refresh_label(LONG_BREAK_MIN)
-#         reps = 0
-#         check_num = ""
-#         timer_label.configure(text="Break", fg=RED)
-#         return
-#     elif reps % 2 != 0:
-#         refresh_label(WORK_MIN)
-#         timer_label.configure(text="Work", fg=GREEN)
-#     else:
-#         refresh_label(SHORT_BREAK_MIN)
-#         check_num += "✔"
-#         timer_label.configure(text="Break", fg=PINK)
-#
-#
-# # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-#
-#
-# def refresh_label(t):
-#     global timer
-#     if t > 0:
-#         mins, secs = divmod(t, 60)
-#         timer = "{:02d}:{:02d}".format(mins, secs)
-#         timer_text.configure(text=timer)
-#         timer = window.after(1000, refresh_label, t-1)
-#     else:
-#         start_timer()
-#         check_label.configure(text=check_num)
-#
-#
-# # ---------------------------- UI SETUP ------------------------------- #
-#
-# window = Tk()
-# window.title("Pomodoro")
-# window.config(padx=100, pady=50, bg=YELLOW)
-#
-# canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-# tomato_img = PhotoImage(file="Suse_Tomate.png")
-# canvas.create_image(100, 112, image=tomato_img)
-# canvas.grid(column=2, row=1)
-#
-#
-# timer_text = Label(text="25:00", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 40, "bold"))
-# timer_text.grid(column=2, row=2)
-#
-# timer_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"), bg=YELLOW, fg=GREEN)
-# timer_label.grid(column=2, row=0)
-#
-# start_button = Button(text="Start", relief="groove", padx=10, bg=YELLOW, activebackground=GREEN, font=(FONT_NAME, 10,
-#                                                                                                        "bold"),
-#                       command=start_all)
-# start_button.grid(column=1, row=3)
-#
-# reset_button = Button(text="Reset", relief="groove", padx=10, bg=YELLOW, activebackground=GREEN, font=(FONT_NAME, 10,
-#                                                                                                        "bold"),
-#                       command=reset_timer)
-# reset_button.grid(column=3, row=3)
-#
-# check_label = Label(text=check_num, font=(FONT_NAME, 10, "bold"), bg=YELLOW, fg=GREEN)
-# check_label.grid(column=2, row=3)
-#
-# window.mainloop()
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
@@ -110,6 +27,7 @@ def reset_timer():
     global check_num
     timer_text.configure(text="00:00")
     timer_label.configure(text="Timer", fg=GREEN)
+    start_button.configure(image=tomato_start)
     reps = 0
     window.after_cancel(timer)
     check_num = ""
@@ -129,22 +47,26 @@ def start_all():
 
 
 def start_timer():
-    global reps
-    global check_num
+    global reps, check_num, TOMATO_SHORT_LIST, tomato_short
+    random_tomato_short = random.choice(TOMATO_SHORT_LIST)
+    tomato_short = PhotoImage(file=random_tomato_short)
     reps += 1
     if reps == 8:
         refresh_label(LONG_BREAK_MIN * 60)
         reps = 0
         check_num = ""
         check_label.configure(text=check_num)
+        start_button.configure(image=tomato_long)
         timer_label.configure(text="Break", fg=RED)
     elif reps % 2 != 0:
         refresh_label(WORK_MIN * 60)
+        start_button.configure(image=tomato_work)
         timer_label.configure(text="Work", fg=GREEN)
     else:
         refresh_label(SHORT_BREAK_MIN * 60)
         check_num += "✔"
         check_label.configure(text=check_num)
+        start_button.configure(image=tomato_short)
         timer_label.configure(text="Break", fg=PINK)
 
 
@@ -169,7 +91,7 @@ def refresh_label(t):
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
-window.title("Pomodoro")
+window.title("Suso Pomodoro")
 window.config(padx=100, pady=10, bg=YELLOW)
 
 timer_text = Label(text="00:00", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 40, "bold"))
@@ -178,8 +100,12 @@ timer_text.grid(column=2, row=2)
 timer_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"), bg=YELLOW, fg=GREEN)
 timer_label.grid(column=2, row=0)
 
-tomato = PhotoImage(file=r"Suse_Tomate.png")
-start_button = Button(image=tomato, activebackground=YELLOW, borderwidth=0, bg=YELLOW, font=(FONT_NAME, 10, "bold"),
+tomato_start = PhotoImage(file=TOMATO_START)
+tomato_work = PhotoImage(file=TOMATO_WORK)
+tomato_long = PhotoImage(file=TOMATO_LONG)
+tomato_short = PhotoImage(file=TOMATO_SHORT_LIST[0])
+
+start_button = Button(image=tomato_start, activebackground=YELLOW, borderwidth=0, bg=YELLOW, font=(FONT_NAME, 10, "bold"),
                       command=start_all)
 start_button.grid(column=2, row=1)
 
--- Day 31 - Flash Card App/main.py
+++ Day 31 - Flash Card App/main.py
@@ -2,11 +2,17 @@ from tkinter import *
 import pandas as pd
 import random as rd
 
-BACKGROUND_COLOR = "#B1DDC6"
+BACKGROUND_COLOR = "#81b29a"
 KNOWN_LANGUAGE = "Deutsch"
 UNKNOWN_LANGUAGE = "Portugiesisch"
-LIST_FILEPATH = "data/word_list_pt-de.csv"
-NEW_LIST_FILEPATH = "data/words_to_learn.csv"
+LIST_FILEPATH = "Lists/word_list_pt-de.csv"
+NEW_LIST_FILEPATH = "Lists/words_to_learn.csv"
+COLUMN_NAMES_IN_FILE = ["pt", "de"]
+CARD_FRONT_IMAGE = "Artwork/card_front.png"
+CARD_BACK_IMAGE = "Artwork/card_back.png"
+WINDOW_TITLE = "Flash Card App"
+BUTTON_IMAGE_WRONG = "Artwork/wrong.png"
+BUTTON_IMAGE_RIGHT = "Artwork/right.png"
 word_tuple = ("", "")
 word = word_tuple[0]
 translation = word_tuple[1]
@@ -33,15 +39,15 @@ def next_card():
     window.after_cancel(flip_timer)
     get_random_word()
     canvas.itemconfigure(card_side, image=card_front)
-    canvas.itemconfigure(language_txt, text=UNKNOWN_LANGUAGE, fill="black")
-    canvas.itemconfigure(word_pt, text=word, fill="black")
+    canvas.itemconfigure(language_txt, text=UNKNOWN_LANGUAGE, fill="midnight blue")
+    canvas.itemconfigure(word_pt, text=word, fill="midnight blue")
     flip_timer = window.after(3000, turn_card)
 
 
 def turn_card():
     canvas.itemconfigure(card_side, image=card_back)
-    canvas.itemconfigure(language_txt, text=KNOWN_LANGUAGE, fill="white")
-    canvas.itemconfigure(word_pt, text=translation, fill="white")
+    canvas.itemconfigure(language_txt, text=KNOWN_LANGUAGE, fill="grey10")
+    canvas.itemconfigure(word_pt, text=translation, fill="grey10")
 
 # ---------------------Button Logic--------------------- #
 
@@ -52,7 +58,7 @@ def unknown_action():
 
 def known_action():
     del vocabulary_dict[word]
-    new_vocabulary_df = pd.DataFrame(vocabulary_dict.items(), columns=["pt", "de"])
+    new_vocabulary_df = pd.DataFrame(vocabulary_dict.items(), columns=COLUMN_NAMES_IN_FILE)
     new_vocabulary_df.to_csv(NEW_LIST_FILEPATH)
     next_card()
 
@@ -60,33 +66,33 @@ def known_action():
 # ---------------------Window--------------------- #
 
 window = Tk()
-window.title("Flash Card App")
-window.configure(bg=BACKGROUND_COLOR, padx=50, pady=50)
+window.title(WINDOW_TITLE)
+window.configure(bg=BACKGROUND_COLOR, padx=50, pady=40)
 
 flip_timer = window.after(3000, turn_card)
 
 # ---------------------Card--------------------- #
 
-canvas = Canvas(width=820, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(column=0, row=0, columnspan=2)
-card_front = PhotoImage(file="images/card_front.png")
-card_back = PhotoImage(file="images/card_back.png")
-card_side = canvas.create_image(420, 263, image=card_front)
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(column=0, row=0, columnspan=2, pady=10)
+card_front = PhotoImage(file=CARD_FRONT_IMAGE)
+card_back = PhotoImage(file=CARD_BACK_IMAGE)
+card_side = canvas.create_image(400, 263, image=card_front)
 
 # ---------------------Text--------------------- #
 
-language_txt = canvas.create_text(410, 150, text=UNKNOWN_LANGUAGE, font=("Tahoma", 40, "italic"))
-word_pt = canvas.create_text(410, 263, text=word, font=("Tahoma", 60, "bold"))
+language_txt = canvas.create_text(400, 75, text=UNKNOWN_LANGUAGE, font=("Lucida Handwriting", 40, "italic"))
+word_pt = canvas.create_text(400, 295, text=word, font=("Lucida Handwriting", 60, "bold"))
 
 # ---------------------Buttons--------------------- #
 
-wrong_img = PhotoImage(file="images/wrong.png")
-button_unknown = Button(image=wrong_img, bd=0, command=unknown_action, highlightthickness=0)
-button_unknown.grid(column=0, row=1)
+wrong_img = PhotoImage(file=BUTTON_IMAGE_WRONG)
+button_unknown = Button(image=wrong_img, bd=0, bg=BACKGROUND_COLOR, command=unknown_action, highlightthickness=0)
+button_unknown.grid(column=0, row=1, pady=10)
 
-right_img = PhotoImage(file="images/right.png")
-button_known = Button(image=right_img, bd=0, command=known_action, highlightthickness=0)
-button_known.grid(column=1, row=1)
+right_img = PhotoImage(file=BUTTON_IMAGE_RIGHT)
+button_known = Button(image=right_img, bd=0, bg=BACKGROUND_COLOR, command=known_action, highlightthickness=0)
+button_known.grid(column=1, row=1, pady=10)
 
 # ---------------------Misc--------------------- #
 
